d815754a80a8a7832cb3c1173076c2e8a45442ae
aishettima <you@example.com>
2025-01-24 00:29:07 +0100

    second commit

---
+++ js/contact.js
@@ -0,0 +1,85 @@
+(function ($) {
+    'use strict';
+
+    var form = $('.contact__form'),
+        message = $('.contact__msg'),
+        form_data;
+
+    // Success function
+    function done_func(response) {
+        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
+        message.text(response);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+        form.find('input:not([type="submit"]), textarea').val('');
+    }
+
+    // fail function
+    function fail_func(data) {
+        message.fadeIn().removeClass('alert-success').addClass('alert-success');
+        message.text(data.responseText);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+    }
+    
+    form.submit(function (e) {
+        e.preventDefault();
+        form_data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'),
+            data: form_data
+        })
+        .done(done_func)
+        .fail(fail_func);
+    });
+    
+})(jQuery);
+
+
+
+
+/*Begin codes for slider */
+
+    var index = 0;
+var slides = document.querySelectorAll(".slides");
+var dot = document.querySelectorAll(".dot");
+
+function changeSlide(){
+
+  if(index<0){
+    index = slides.length-1;
+  }
+  
+  if(index>slides.length-1){
+    index = 0;
+  }
+  
+  for(let i=0;i<slides.length;i++){
+    slides[i].style.display = "none";
+    dot[i].classList.remove("active");
+
+
+ /*New */
+    dot[i].style.backgroundColor="";
+  }
+  
+  slides[index].style.display= "block";
+  dot[index].classList.add("active");
+
+  /*New */
+  dot[index].style.backgroundColor="#f75757";
+  
+  index++;
+  
+  setTimeout(changeSlide,3000);
+  
+}
+
+changeSlide();
+
+
+/*Ens  codes for slider */
+   
